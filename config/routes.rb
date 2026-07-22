0912c72bebcd638fbe580134418e352577a5692b
bolitaria <bolitariaa@gmail.com>
2026-07-22 18:20:42 +0200

    style: fix RuboCop offenses in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :products, only: [:index, :show]
+  resources :products, only: [ :index, :show ]
   resources :products, only: [] do
-    resource :configuration, only: [:new, :update], controller: "configurations"
+    resource :configuration, only: [ :new, :update ], controller: "configurations"
   end
   root "products#index"
 end
